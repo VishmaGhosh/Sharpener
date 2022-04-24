4a883f69a5774b9fa60a9c0ed4abd5cccedcbee2
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-04-24 18:57:26 +0530

    Update amount

--- React/01-starting-setup/src/Components/Expenses/ExpenseDetails.js
+++ React/01-starting-setup/src/Components/Expenses/ExpenseDetails.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import './ExpenseItems.css'
-const  ExpenseDetails = (props) => {
+const ExpenseDetails = (props) => {
+  const [title, setTitle] = useState(props.title)
+  const [amount, setAmount] = useState(props.amount)
+
+  const clickHandler = () => {
+    setTitle("Updated !!")
+    console.log("Clicked !");
+  }
+  const updateAmount = () => {
+    setAmount(100)
+  }
   return (
-      <div className='expense-item__description'>
-          <h2>{props.title}</h2>
-          <div className='expense-item__price'>{props.amount}</div>
+    <div className='expense-item__description'>
+      <h2>{title}</h2>
+      <div className='expense-item__price'>{amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button>Delete Expense</button>
+      <button onClick={updateAmount}>update amount</button>
     </div>
   )
 }
