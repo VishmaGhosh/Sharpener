1b46cf865b7739308a0fb59655bd057d81bd0262
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-05-05 15:20:33 +0530

    add Modal

--- React/Restaurent App/01-starting-project/src/components/Cart/Cart.js
+++ React/Restaurent App/01-starting-project/src/components/Cart/Cart.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
+
 const Cart = () => {
-  return (
-    <div className={classes.cart}>
-        <div>Your Cart</div>
-        <div>0</div>
-    </div>
-  )
+    const cartItem = (
+        <ul className={classes['cart-items']}>
+            {
+                [
+                    { id: 'c1', name: 'Sushi', amount: '2', price: "30.21" }
+                ].map((item) => <li>{item.name}</li>)
+            }
+        </ul>
+    )
+    return (
+        <Modal>
+            {cartItem}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>$35</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+
+        </Modal>
+    )
 }
 
 export default Cart
