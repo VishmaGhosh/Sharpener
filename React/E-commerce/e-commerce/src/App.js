580cfa1ce6249148fc1d40d184d8d661e3ac13b8
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-05-11 21:18:55 +0530

    Add routing

--- React/E-commerce/e-commerce/src/App.js
+++ React/E-commerce/e-commerce/src/App.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
-import { Fragment } from 'react';
 import Header from './components/layout/Header';
 import Products from './components/product/Products';
 import Footer from './components/layout/Footer';
 import Cart from './components/cart/Cart';
+import About from './components/layout/About'
+import CartContextProvider from './store/CartContextProvider';
+import ProductSummary from './components/product/ProductSummary';
 
 function App() {
   const [showCart, setShowCart] = useState(false)
@@ -12,14 +15,18 @@ function App() {
     setShowCart(!showCart);
   } 
   return (
-    <Fragment>
+    <CartContextProvider>
       <Header showCart={ showCartHandler} />
       <main>
+        <ProductSummary />
+        <Route path="/about" exact>
+          <About />
+        </Route>
         { showCart && <Cart showCart={showCartHandler} /> }
         <Products />
       </main>
       <Footer />
-    </Fragment>
+    </CartContextProvider>
   );
 }
 
