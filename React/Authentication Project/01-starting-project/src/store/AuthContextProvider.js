c655207da6b8c0e08cf758daead6220e7c170d69
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-05-14 18:35:19 +0530

    store the token in localstorage.

--- React/Authentication Project/01-starting-project/src/store/AuthContextProvider.js
+++ React/Authentication Project/01-starting-project/src/store/AuthContextProvider.js
@@ -1,15 +1,18 @@
 import React,{useState} from 'react'
 import authContext from './auth-context.js'
 const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken);
 
     const userLoggedIn = !!(token);
     const loginHandler = (token) => {
         setToken(token);
+        localStorage.setItem("token", token);
     }
 
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     }
 
     const authContextValue = {
