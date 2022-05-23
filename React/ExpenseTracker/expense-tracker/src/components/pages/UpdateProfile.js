31976a5af7b7ad22cc31d38931027e88df463fe0
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-05-23 19:01:54 +0530

    verify email

--- React/ExpenseTracker/expense-tracker/src/components/pages/UpdateProfile.js
+++ React/ExpenseTracker/expense-tracker/src/components/pages/UpdateProfile.js
@@ -1,4 +1,4 @@
-import React,{useRef, useContext, useEffect} from 'react'
+import React,{useRef, useContext, useEffect, useState} from 'react'
 import authContext from '../../store/auth-context';
 import classes from './UpdateProfile.module.css'
 
@@ -6,6 +6,9 @@ const UpdateProfile = () => {
     const authCtx = useContext(authContext);
     const userNameRef = useRef();
     const photoUrlRef = useRef();
+    const codeRef = useRef();
+
+    const [confirmPage, setConfirmPage] = useState(false);
 
     useEffect(() => {
         fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBfYZu61DWKwje2272Eom3nTMqkXkTLucg', {
@@ -24,7 +27,7 @@ const UpdateProfile = () => {
                 userNameRef.current.value = data.users[0].displayName
                 photoUrlRef.current.value = data.users[0].photoUrl
             })
-        .catch(err => console.log(err))
+            .catch(err => console.log(err))
     }, [])
 
     const submitHandler = (e) => {
@@ -61,6 +64,79 @@ const UpdateProfile = () => {
                 alert(err.message)
             })
     }
+
+    const confirmEmailHandler = (e) => {
+        e.preventDefault();
+
+        setConfirmPage(true);
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBfYZu61DWKwje2272Eom3nTMqkXkTLucg', {
+            method: "POST",
+            body: JSON.stringify({
+                requestType: "VERIFY_EMAIL",
+                idToken: authCtx.token,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                else {
+                    return res.json().then(data => {
+                        // Error Modal
+                        let errorMsg = 'Authenticate is Failed'
+                        throw new Error(errorMsg);
+                    })
+                }
+            }).then(data => {
+                console.log(data);
+
+            }).catch(err => {
+                alert(err.message)
+            })
+
+    }
+
+    const confirmEmail = (e) => {
+        e.preventDefault();
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBfYZu61DWKwje2272Eom3nTMqkXkTLucg', {
+            method: "POST",
+            body: JSON.stringify({
+                oobCode: codeRef.current.value
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        })
+            .then(res => {
+                if (res.ok) {
+                    setConfirmPage(false);
+                    return res.json();
+                }
+                else {
+                    return res.json().then(data => {
+                        // Error Modal
+                        let errorMsg = 'Authenticate is Failed'
+                        throw new Error(errorMsg);
+                    })
+                }
+            }).then(data => {
+                console.log(data);
+
+            }).catch(err => {
+                alert(err.message)
+            })
+    }
+    const Page = <div>
+        <input type='text' ref={codeRef} />
+        <button onClick={confirmEmail}>Verify Code</button>
+    </div>
+
   return (
       <div className={classes.userDetails}>
           <h2>Contact Details</h2>
@@ -70,7 +146,9 @@ const UpdateProfile = () => {
               <label>Profile Photo Url</label>
               <input type='text' ref={photoUrlRef} /><br />
               <button type='submit'>Update</button>
+              <button onClick={confirmEmailHandler}>Comfirm Email</button>
           </form>
+          {confirmPage && Page}
     </div>
   )
 }
