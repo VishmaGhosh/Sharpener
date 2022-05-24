0743f5ea72af2746ef5cee1c2245a0290d390b02
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-05-24 10:34:15 +0530

    add logout

--- React/ExpenseTracker/expense-tracker/src/components/layout/Header.js
+++ React/ExpenseTracker/expense-tracker/src/components/layout/Header.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import authContext from '../../store/auth-context'
 import classes from './Header.module.css'
+import { useHistory } from 'react-router-dom'
+
 const Header = (props) => {
+    const authCtx = useContext(authContext);
+
+    const history = useHistory();
+    const logoutHandler = (e) => {
+        e.preventDefault();
+        authCtx.logout();
+        history.replace("/login");
+    }
     return (
         <Fragment>
             <header className={classes.header}>
                 <NavLink  to='/'>Home</NavLink>
                 <NavLink to='/login'>Login</NavLink>
                 <NavLink to='/signup'>Signup</NavLink>
+                <button onClick={logoutHandler}>Logout</button>
             </header>
         </Fragment>
     )
